5d08cecc2faaa1d488812f4fde3e4224e03797f1
Hannah Hawkins <55900883+HHawkins2520@users.noreply.github.com>
2021-03-29 19:38:37 -0400

    Update main.py
    Transferred TODO comments to project's To Do column

--- src/main.py
+++ src/main.py
@@ -1,8 +1,3 @@
 from src import *
 
-# TODO: Transaction log
-# TODO: Check number inputs for symbols
-# TODO: Don't allow budget amounts > total balance
-# TODO: Save and load current state
-
 WINDOW.mainloop()
